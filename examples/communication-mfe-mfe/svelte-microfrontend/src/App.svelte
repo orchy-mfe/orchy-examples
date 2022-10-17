1159487ee876b28c915bcb5f65e3f2bf95b83d1b
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-10-17 11:01:05 +0200

    fix: custom history

--- examples/communication-mfe-mfe/svelte-microfrontend/src/App.svelte
+++ examples/communication-mfe-mfe/svelte-microfrontend/src/App.svelte
@@ -1,12 +1,14 @@
 <script>
-  import {Router} from 'svelte-navigator'
+  import {Router, createHistory} from 'svelte-navigator'
   import SvelteLogo from './assets/svelte.svelte'
   import Counter from './lib/Counter.svelte'
+
+  const history = createHistory(window)
 
   export let eventBus, baseUrl
 </script>
 
-<Router basepath={baseUrl}>
+<Router basepath={baseUrl} history={history}>
   <SvelteLogo />
 
   <div class="card">
