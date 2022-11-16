037167f0180d4cf7044bd00bd8eedf0c7c59f8ef
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-11-16 15:50:46 +0100

    fix: routing

--- examples/routing-mfe/svelte-microfrontend/src/main.js
+++ examples/routing-mfe/svelte-microfrontend/src/main.js
@@ -4,6 +4,7 @@ import App from './App.svelte'
 import OrchyMicroFrontend from '@orchy-mfe/spa-adapter'
 export class SvelteMfe extends OrchyMicroFrontend {
   async mount(microFrontendProperties) {
+    location.hash = microFrontendProperties?.basePath
     this.app = new App({
       target: this.getContainer(),
       props: microFrontendProperties
