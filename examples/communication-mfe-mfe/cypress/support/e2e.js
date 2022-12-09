554fc0c82796e0a2f16628fc60158a7d9cd1f68a
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-12-09 23:11:51 +0100

    feat: mfe-import-maps

--- examples/communication-mfe-mfe/cypress/support/e2e.js
+++ examples/communication-mfe-mfe/cypress/support/e2e.js
@@ -1,9 +1,1 @@
 import './commands'
-
-Cypress.Commands.add('orchyConfigJson', () => cy.fixture('orchy-config.json'))
-Cypress.Commands.add('orchyConfigHtml', () => {
-    const fixtureContent = cy.fixture('orchy-config.json')
-    return fixtureContent.replace('.json', '.html')
-})
-Cypress.Commands.add('pageConfigJson', () => cy.fixture('page-config.json'))
-Cypress.Commands.add('pageConfigHtml', () => cy.fixture('page-config.html'))
---
+++ examples/mfe-import-maps/cypress/e2e/e2e.cy.js
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+describe('mfe-import-maps', () => {
+  const importMaps = () => {
+    cy.findByText('count is 0').click()
+    cy.findByText('count is 1').click()
+    cy.findByText('count is 2')
+  }
+
+  describe('json page config', () => {
+    beforeEach(() => {
+      cy.fixture('orchy-config.json').then(orchyConfigJson => {
+        cy.intercept('/api/v1/configuration/orchy-config.json', {
+          statusCode: 200,
+          body: orchyConfigJson
+        })
+      })
+
+      cy.fixture('page-config.json').then(pageConfigJson => {
+        cy.intercept('/api/v1/configuration/page-config.json', {
+          statusCode: 200,
+          body: pageConfigJson
+        })
+      })
+
+      cy.visit('/')
+    })
+  
+    it('import maps', importMaps)
+  })
+
+  describe('html page config', () => {
+    beforeEach(() => {
+      cy.fixture('orchy-config.json').then(orchyConfigJson => {
+        orchyConfigJson.microPages['/'].pageConfiguration = 'page-config.html'
+        cy.intercept('/api/v1/configuration/orchy-config.json', {
+          statusCode: 200,
+          body: orchyConfigJson
+        })
+      })
+
+      cy.fixture('page-config.html').then(pageConfigHtml => {
+        cy.intercept('/api/v1/configuration/page-config.html', {
+          statusCode: 200,
+          body: pageConfigHtml
+        })
+      })
+
+      cy.visit('/')
+    })
+  
+    it('import maps', importMaps)
+  })
+})
